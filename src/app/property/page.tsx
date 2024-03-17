0fc69089100d23c578b0eee3149d49e48d1aec51
Shivansh <shivanshrocks21@gmail.com>
2024-03-17 21:55:26 +0530

    page

---
+++ src/app/property/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import ImageSlider from "@/components/carousals/ImageSlider";
+import { SectionLayout } from "@/components/layouts/SectionLayout";
+import React from "react";
+import {ThreeDCardDemo} from '@/components/Card'
+
+const Property = () => {
+	return (
+		<>
+            <ThreeDCardDemo />
+        </>
+	);
+};
+
+export default Property;
